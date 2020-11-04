d1e5d4d3b0bb9c0fdcbe169d454a3abf0aa9d75e
Peter Puhov <peterpuhov.github@gmail.com>
2020-11-04 15:38:25 -0500

    Added as submodule

--- run_dikeCS.sh
+++ run_dikeCS.sh
@@ -3,7 +3,7 @@
 docker run -it --rm  \
 -p 9000:9000 \
 -v "$(pwd)/build/Release/"://usr/local/bin \
--v "/mnt/usb-SanDisk_Ultra_128GB/dike/minio/data":/data \
+-v "$(pwd)/../minio/data":/data \
 --network dike-net \
 --name dikeCS \
 ubuntu:20.04 dikeCS
