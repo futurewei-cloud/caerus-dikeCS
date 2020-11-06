93c7dbf68a2ec6ff8ce084bdaf5b2cd66274d9c8
Peter Puhov <peterpuhov.github@gmail.com>
2020-11-06 09:31:38 -0500

    Support for older cmakes

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@
 
 BUILD_TYPE=Release
 
+if [ $(cmake -P CMakeCheckVersion.txt) == "OK" ]; then
 cmake -D CMAKE_BUILD_TYPE=${BUILD_TYPE} -S . -B build/${BUILD_TYPE}
 cmake --build ./build/${BUILD_TYPE}
+else
+mkdir -p build/${BUILD_TYPE}
+cd build/${BUILD_TYPE}
+cmake -D CMAKE_BUILD_TYPE=${BUILD_TYPE} ../..
+cd ../../
+fi
 
--- external/build_aws.sh
+++ external/build_aws.sh
@@ -2,10 +2,30 @@
 
 export INSTALL_PATH=$(pwd)/build-aws
 
+if [ $(cmake -P CMakeCheckVersion.txt) == "OK" ]; then
 cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${INSTALL_PATH} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -S aws-c-common -B aws-c-common/build
 cmake --build aws-c-common/build --target install
 cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${INSTALL_PATH} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -S aws-checksums -B aws-checksums/build
 cmake --build aws-checksums/build --target install
 cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${INSTALL_PATH} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} -S aws-c-event-stream -B aws-c-event-stream/build
 cmake --build aws-c-event-stream/build --target install
+
+else
+mkdir -p aws-c-common/build
+cd aws-c-common/build
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${INSTALL_PATH} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} ..
+make && make install
+cd ../../
+
+mkdir -p aws-checksums/build
+cd aws-checksums/build
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${INSTALL_PATH} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} ..
+make && make install
+cd ../../
+
+mkdir -p aws-c-event-stream/build
+cd aws-c-event-stream/build
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=${INSTALL_PATH} -DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} ..
+make && make install
+fi
 
