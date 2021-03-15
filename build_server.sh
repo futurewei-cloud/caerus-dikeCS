20eebb4cb0aa20d8e8df1a3338776935cd2f1f05
Robert Foley <robfoley972@gmail.com>
2021-03-15 09:24:30 -0400

    Added top level build script build.sh

---
+++ build_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BUILD_TYPE=Release
+
+if [[ $(cmake -P CMakeCheckVersion.txt) == "OK" ]];
+then
+    cmake -D CMAKE_BUILD_TYPE=${BUILD_TYPE} -S . -B build/${BUILD_TYPE}
+    cmake --build ./build/${BUILD_TYPE}
+else
+    mkdir -p build/${BUILD_TYPE}
+    cd build/${BUILD_TYPE}
+    cmake -D CMAKE_BUILD_TYPE=${BUILD_TYPE} ../..
+    make
+    cd ../../
+fi
+
